92c154d2c7915cc0cd38376eddf40ac90252351c
olisax <olisax@users.noreply.github.com>
2025-03-05 10:33:57 +0100

    Update CSV_to_OTC.py
    Add some flexibility on command-line arguments

--- CSV_to_OTC.py
+++ CSV_to_OTC.py
@@ -4,6 +4,30 @@ Convert "KepServer" CSV file to "OPC Quick Client" OTC file - by Olivier
 """
 import binascii, os, sys
 
+def display_help(): #descrizione del programma.
+    help_text = """
+    Convert "KepServer" CSV file to "OPC Quick Client" OTC file for a quick visualization of all tags in a datalogger.
+    
+    Usage:
+    - In KepServer, go to Data Logger, select Data Map and click on "Export CSV..."
+    - Save this CSV file to a folder on your local computer.
+    - Run CSV_to_OTC in this folder to generate an OTC file. If older OTC files exist, they will be overwritten.
+    - Open "OPC Quick Client". File, Open... and select the OTC file.
+
+    Options:
+    - No arguments: Process all .csv files in the current directory.
+    - 1 argument:
+      * If it's a .csv filename: Process the specific file in the current directory.
+      * If it's a directory: Process all .csv files in the directory.
+    - 2 arguments:
+      * Arguments can be in any order: .csv filename and directory.
+      * Process the specific file in the specified directory.
+    """
+    print(help_text)
+    
+def is_directory(path): #check if the path is a directory by checking if it contains '/' or '\\'.
+    return '/' in path or '\\' in path
+
 def TypeToHex(tipo): #convertire il tipo in un carattere hexadecimale
     if tipo=="Default":
         return "00" #hex(0)[2:].zfill(2)
@@ -68,13 +92,73 @@ def CSVtoOTC(FileAdress):
             file_out.write(binascii.unhexlify(TipoItems[i])) #tipo 
             file_out.write(binascii.unhexlify(BlockItem3))
 
-FolderName = os.getcwd() #get current dir
-print(FolderName)  
-if len(sys.argv) > 1:   # If a filename is provided as an argument, open that file
-    FileName = sys.argv[1]
+FileName = None
+FolderName = None           
+args = sys.argv[1:] #Get command-line arguments
+            
+if len(args) == 1:
+    if sys.argv[1].endswith(".csv"):
+        if os.path.isfile(sys.argv[1]):
+            FileName = sys.argv[1]
+        else:
+            print(f"File '{sys.argv[1]}' not found in the current directory.")
+            exit()
+    elif is_directory(sys.argv[1]):
+        FolderName = sys.argv[1]
+    else:
+        display_help()
+        exit()
+            
+elif len(args) == 2:
+    for arg in args:
+        if arg.endswith(".csv"):
+            FileName = arg
+        elif is_directory(arg):
+            FolderName = arg
+    if FileName and FolderName:
+        if os.path.isfile(FolderName+"\\"+FileName) == False:
+            print(f"File '{FileName}' not found in directory '{FolderName}'.")
+            exit()
+    else:
+        display_help()
+        exit()
+    
+if FolderName == None:        
+    FolderName = os.getcwd() #get current dir     
+            
+if FileName:   #if a filename is provided as an argument, open that file
     CSVtoOTC(FolderName+"\\"+FileName)  
-else:  # If no argument is provided, open all CSV in the current directory
+else:  #if no argument is provided, open all CSV in the current directory
     for FileName in os.listdir(FolderName):
         if FileName.endswith(".csv"): #select a folder and take all csv files in it
             print(FileName)
             CSVtoOTC(FolderName+"\\"+FileName)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
